ab6ec7724f1b24b8693cff0772ada8822712bb21
DavidmKlunk <98335929+DavidmKlunk@users.noreply.github.com>
2024-02-06 18:13:25 -0700

    Working webpage with static image

--- portfolio/portfolio_app/views.py
+++ portfolio/portfolio_app/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('home page')
+    #return HttpResponse('home page')
+    return render( request, 'portfolio_app/index.html')
