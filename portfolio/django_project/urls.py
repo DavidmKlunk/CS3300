fe2294c0406f7d3d20ebdddf35da026f62e2c818
DavidmKlunk <98335929+DavidmKlunk@users.noreply.github.com>
2024-03-03 17:21:21 -0700

    final commit

--- portfolio/django_project/urls.py
+++ portfolio/django_project/urls.py
@@ -15,9 +15,45 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
+    #next 4 paths, admin/password_reset, admin/password_reset/done/, reset/<uidb64>/<token>/, and reset/done/
+    #provide for a reset password option at the admin login screen
+    
+    path("admin/password_reset/",
+         auth_views.PasswordResetView.as_view(
+             extra_context={"site_header": admin.site.site_header}
+         ),
+         name="admin_password_reset",
+         ),
+    
+    path(
+        "admin/password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            extra_context={"site_header": admin.site.site_header}
+        ),
+        name="password_reset_done",
+    ),
+
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            extra_context={"site_header": admin.site.site_header}
+        ),
+        name="password_reset_confirm",
+    ),
+
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            extra_context={"site_header": admin.site.site_header}
+        ),
+        name="password_reset_complete",
+    ),
+    
+    
     path('admin/', admin.site.urls),
 
     #connect path to portfolio_app urls
